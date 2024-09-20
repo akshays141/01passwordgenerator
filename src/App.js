ff8b38c4e85dcfca2f8ddf7f2a528a95d362be09
akshays141 <akshays141@gmail.com>
2024-09-20 17:02:29 +0530

    passwordgenerator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
+import { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  
+  const passwordRef= useRef(null);
+
+  const passwordGenerator = useCallback(()=>{
+    let pass="";
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numberAllowed) str+="0123456789"
+    if(charAllowed) str+="!@#$%^&*(){}[]-+_=`~"
+
+    for (let i = 1; i <length; i++) {
+      let char = Math.floor(Math.random() * str.length +1)
+      pass+=str.charAt(char);
+    }
+
+    setPassword(pass);
+
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,101);
+
+    window.navigator.clipboard.writeText(password);
+
+  },[password]);
+
+
+  useEffect(()=>{
+     passwordGenerator()
+  },[length,numberAllowed,charAllowed,passwordGenerator])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className='text-center text-4xl'>Password Generator</h1>
     </div>
   );
 }
